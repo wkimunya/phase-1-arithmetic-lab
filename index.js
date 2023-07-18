59971f71db178f3a74d8ad3d3315e237329cea61
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-18 17:46:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numbers = [5, 10, 20, 15];
+const max = Math.max(...numbers);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
+
+
+
+
